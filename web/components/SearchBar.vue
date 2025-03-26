39e8f0b0fa6904789856197f182d6d72d497ddcc
Brice Ledanois <ledanoisb@gmail.com>
2025-03-26 09:31:14 +0100

    ✨ Ajoute la configuration de Tailwind CSS et améliore le composant de recherche

--- web/components/SearchBar.vue
+++ web/components/SearchBar.vue
@@ -3,3 +3,9 @@
         <input type="text" class="w-full placeholder-indigo-900 bg-indigo-200 rounded-3xl h-14 px-6" placeholder="Search for a product, a brand...">
     </div>
 </template>
+
+<style scoped>
+    input{
+        font-family: outfit;
+    }
+</style>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,10 +1,18 @@
 <template>
     <SearchBar />
     <section class="mt-10">
-        <div class="bg-indigo-600 h-32 mx-5 rounded-[20px]" style="background-image: url('public/images/moutain-rider.jpg');">
+        <div class="bg-indigo-600 min-h-52 mx-5 rounded-[20px] bg-cover bg-center bg-no-repeat p-5 flex justify-between flex-col" 
+             style="background-image: url('/images/moutain-rider.jpg');">
+             <h2 class="text-indigo-600 text-2xl w-[50%]">Discover the Electra bikes</h2>
+             <button class="bg-indigo-600 text-indigo-50 py-2 rounded-[20px] w-[50%]">
+                Discover
+             </button>
         </div>
     </section>
 </template>
 
-<script setup>
-</script>
+<style scoped>
+    h2, button{
+        font-family: NeuePower;
+    }
+</style>
