cb3a9926cdf34517a01d58a3aa145e8b43ff4624
Brice Ledanois <ledanoisb@gmail.com>
2025-03-26 10:04:30 +0100

    ✨ Ajoute un composant MobileCard et améliore la mise en page de la page d'accueil

---
+++ web/components/MobileCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- Product Card Mobile -->
+    <div>
+        <h3>product</h3>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    product: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <SearchBar />
-    <section class="mt-10">
+    <!-- Les 3 boutons d'accueil -->
+    <section class="mt-10 mb-10">
         <div class="bg-indigo-600 min-h-52 mx-5 rounded-[20px] bg-cover bg-center bg-no-repeat p-5 flex justify-between flex-col" 
              style="background-image: url('/images/moutain-rider.jpg');">
              <h2 class="text-indigo-600 text-2xl w-[50%]">Discover the Electra bikes</h2>
@@ -9,13 +10,22 @@
              </button>
         </div>
         <div class="grid grid-cols-2 mt-5 gap-5">
-            <div class="bg-indigo-600 ml-5 rounded-[20px] aspect-square">
-
+            <div class="bg-indigo-600 ml-5 rounded-[20px] aspect-square bg-cover bg-center bg-no-repeat p-5"
+            style="background-image: url('/images/look-sky.png');">
+                <h2 class="text-indigo-600 text-2xl">Our Stores</h2>
             </div>
-            <div class="bg-indigo-600 mr-5 rounded-[20px] aspect-square">
-
+            <div class="bg-indigo-600 mr-5 rounded-[20px] aspect-square bg-cover bg-center bg-no-repeat p-5"
+            style="background-image: url('/images/grass-bike.jpg');">
+                <h2 class="text-indigo-600 text-2xl">Road Bikes</h2>
             </div>
         </div>
+    </section>
+    <!-- Des Produits mis en avant -->
+    <section class="grid grid-cols-2 px-5 gap-5">
+        <!-- Mobile Card -->
+        <div class="bg-indigo-600 rounded-[20px] h-2 "></div>
+
+
     </section>
 </template>
 
