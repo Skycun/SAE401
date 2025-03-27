78b5cd7bfd680b5a4bd843217560bf970c3399a9
Brice Ledanois <ledanoisb@gmail.com>
2025-03-27 10:05:36 +0100

    ✨ Ajoute une barre de recherche et des détails sur le produit dans la page produit

--- web/pages/product/[id].vue
+++ web/pages/product/[id].vue
@@ -1,4 +1,15 @@
 <template>
+    <SearchBar />
+
+    <div class="bg-white min-h-[60vh] mt-10 m-5 rounded-[20px] p-5">
+        <img src="../../public/images/packshots/c-ps2.png" alt="Image de vélo" class="aspect-square object justify-center mx-auto mb-3">
+        <h3 class="text-3xl">{{ product.product_name }}</h3>
+        <h3 class="text-indigo-600 text-3xl">{{ product.list_price }}$</h3>
+        <p>{{ product.brand.brand_name }} - {{ product.model_year }}</p>
+        <p>{{ product.category.category_name }}</p>
+        <p class="text-indigo-600 text-lg">in Stock</p>
+    </div>
+
     <div>
         <h1>Product page {{ route.params.id }}</h1>
         <p>{{ product.product_name }}</p>
