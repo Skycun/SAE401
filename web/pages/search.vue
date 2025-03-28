807926550fea14d04b8185baaa174e04238ae7f7
Brice Ledanois <ledanoisb@gmail.com>
2025-03-28 10:27:59 +0100

    ✨ Ajoute un composant UCollapsible dans la page de recherche

---
+++ web/pages/search.vue
@@ -0,0 +1,16 @@
+<template>
+    <UCollapsible class="flex flex-col gap-2 w-48">
+      <UButton
+        label="Open"
+        color="neutral"
+        variant="subtle"
+        trailing-icon="i-lucide-chevron-down"
+        block
+      />
+  
+      <template #content>
+        <Placeholder class="h-48" />
+      </template>
+    </UCollapsible>
+  </template>
+  
