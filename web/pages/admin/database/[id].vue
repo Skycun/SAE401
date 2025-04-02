a09876375f9ad0d6dcb606254e93800590c81719
Brice Ledanois <ledanoisb@gmail.com>
2025-04-02 08:42:58 +0200

    ✨ Ajoute la gestion des informations des employés dans l'interface admin

--- web/pages/admin/database/[id].vue
+++ web/pages/admin/database/[id].vue
@@ -5,11 +5,33 @@
     <div v-else>
         <div class="m-5 bg-white rounded-[20px] p-5 mt-10">
             <h2 class="text-indigo-950 text-2xl flex justify-center items-center mb-5">Action</h2>
-            <USelect label="Select Category" v-model="mode" :items="actions" placeholder="Select an action" class="w-full mb-2"/>
+            <USelect label="Select Category" v-model="mode" :items="actions" placeholder="Select an action"
+                class="w-full mb-2" />
         </div>
         <div v-if="mode == 'update'" class="m-5 bg-white rounded-[20px] p-5 ">
             <h2 class="text-indigo-950 text-xl flex justify-center items-center mb-5">Select the {{ table }}</h2>
-            <USelectMenu label="Select {{ table }}" v-model="itemToEdit" :items="editFetch" placeholder="Select an action" class="w-full mb-2"/>
+            <USelectMenu label="Select {{ table }}" v-model="itemToEdit" :items="editFetch"
+                placeholder="Select an action" class="w-full mb-2" />
+        </div>
+        <div v-if="table == 'employees'" class="m-5 bg-white rounded-[20px] p-5 flex flex-col gap-5">
+            <h2 class="text-indigo-950 text-xl flex justify-center items-center mb-5">Fill informations</h2>
+            <UFormField label="Full name" required>
+                <UInput placeholder="Enter full name" class="w-full"/>
+            </UFormField>
+            <UFormField label="Email" required>
+                <UInput placeholder="Enter email" class="w-full"/>
+            </UFormField>
+            <UFormField label="Password" required>
+                <UInput placeholder="Enter password" class="w-full"/>
+            </UFormField>
+            <UFormField label="Role" required>
+                <USelect label="Select Role" :items="[{ label: 'Role 1', value: 'store1' }, { label: 'Role 2', value: 'store2' }]" 
+                placeholder="Select a Role" class="w-full mb-2" />
+            </UFormField>
+            <UFormField label="Store" required>
+                <USelect label="Select Store" :items="[{ label: 'Store 1', value: 'store1' }, { label: 'Store 2', value: 'store2' }]" 
+                placeholder="Select a store" class="w-full mb-2" />
+            </UFormField>
         </div>
 
 
@@ -23,7 +45,7 @@
 
 //Prévenir des gens qui n'ont pas le cookie de connection
 const user_data = useCookie('user_data');
-if(!user_data.value){
+if (!user_data.value) {
     const router = useRouter();
     router.push('/login');
 }
@@ -47,7 +69,7 @@ const actions = ref([
 const route = useRoute();
 const table = ref(route.params.id);
 
-if(route.params.id != 'brands' && route.params.id != 'categories' && route.params.id != 'products' && route.params.id != 'stocks' && route.params.id != 'stores' && route.params.id != 'employees'){
+if (route.params.id != 'brands' && route.params.id != 'categories' && route.params.id != 'products' && route.params.id != 'stocks' && route.params.id != 'stores' && route.params.id != 'employees') {
     throw createError({ statusCode: 404, statusMessage: 'ID invalide' });
 }
 
@@ -56,50 +78,50 @@ const itemToEdit = ref(null);
 const editFetch = ref('');
 
 watch(mode, async (newMode) => {
-  if(newMode === 'update') {
-    const { data } = await useFetch(`https://mirrorsoul.alwaysdata.net/sae401/API/API/${table.value}`);
-    console.log(data.value);
-    switch(table.value) {
-      case 'brands':
-        editFetch.value = data.value.map(brand => ({
-          label: brand.brand_name,
-          value: brand.brand_id
-        }));
-        break;
-      case 'categories':
-        editFetch.value = data.value.map(category => ({
-          label: category.category_name,
-          value: category.category_id
-        }));
-        break;
-      case 'products':
-        editFetch.value = data.value.map(product => ({
-          label: product.product_name,
-          value: product.product_id
-        }));
-        break;
-      case 'stocks':
-        editFetch.value = data.value.map(stock => ({
-          label: stock.stock_id,
-          value: stock.stock_id
-        }));
-        break;
-      case 'stores':
-        editFetch.value = data.value.map(store => ({
-          label: store.store_name,
-          value: store.store_id
-        }));
-        break;
-      case 'employees':
-        editFetch.value = data.value.map(employee => ({
-          label: employee.employees_name,
-          value: employee.employees_id
-        }));
-        break;
+    if (newMode === 'update') {
+        const { data } = await useFetch(`https://mirrorsoul.alwaysdata.net/sae401/API/API/${table.value}`);
+        console.log(data.value);
+        switch (table.value) {
+            case 'brands':
+                editFetch.value = data.value.map(brand => ({
+                    label: brand.brand_name,
+                    value: brand.brand_id
+                }));
+                break;
+            case 'categories':
+                editFetch.value = data.value.map(category => ({
+                    label: category.category_name,
+                    value: category.category_id
+                }));
+                break;
+            case 'products':
+                editFetch.value = data.value.map(product => ({
+                    label: product.product_name,
+                    value: product.product_id
+                }));
+                break;
+            case 'stocks':
+                editFetch.value = data.value.map(stock => ({
+                    label: stock.stock_id,
+                    value: stock.stock_id
+                }));
+                break;
+            case 'stores':
+                editFetch.value = data.value.map(store => ({
+                    label: store.store_name,
+                    value: store.store_id
+                }));
+                break;
+            case 'employees':
+                editFetch.value = data.value.map(employee => ({
+                    label: employee.employees_name,
+                    value: employee.employees_id
+                }));
+                break;
+        }
+    } else {
+        editFetch.value = null;
+        itemToEdit.value = null;
     }
-  } else {
-    editFetch.value = null; 
-    itemToEdit.value = null;
-  }
 })
 </script>
